c2d0cefefc9eb249c7fcd429d51ce623c437c8a9
ngxson <thichthat@gmail.com>
2016-06-14 00:46:36 +0700

    test data

---
+++ next/products/data.js
@@ -0,0 +1,42 @@
+[
+  {  
+    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/ALF.jpg",
+    "name":"Iridescent Prom 2016",
+    "last":"Lorem ipsum dolor sit amet, in velit iudico mandamus sit persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.",
+    "email":"jhony@site.com",
+    "phone":"1-555-222-333",
+    "web":"http://jhonSmith.com"
+  },
+  {  
+    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/alice.jpg",
+    "name":"Carla",
+    "last":"Doe",
+    "email":"carladoe@site.com",
+    "phone":"1-333-111-555",
+    "web":"http://carladoe.com"
+  },
+  {  
+    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/ALF.jpg",
+    "name":"Stefan",
+    "last":"Varela",
+    "email":"stefano@site.com",
+    "phone":"2-444-222-777",
+    "web":"http://stefano.com"
+  },
+  {  
+    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/alice.jpg",
+    "name":"Angeline",
+    "last":"Julai",
+    "email":"angeline@site.com",
+    "phone":"2-555-555-555",
+    "web":"http://angeline.com"
+  },
+  {  
+    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/ALF.jpg",
+    "name":"New name",
+    "last":"for test",
+    "email":"angeline@site.com",
+    "phone":"2-555-555-555",
+    "web":"http://angeline.com"
+  }
+]
