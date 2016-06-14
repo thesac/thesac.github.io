3522abf13701bbfb22d4e82781f3c2a917336cd2
ngxson <thichthat@gmail.com>
2016-06-15 00:45:25 +0700

    data test

--- next/products/data.js
+++ next/products/data.js
@@ -4,44 +4,26 @@
     "name":"Iridescent Prom 2016",
     "last":"Prom thường niên do SAC tổ chức. Sự kiện được thực hiện với sự giúp đỡ của hơn 40 tình nguyện viên. Đã có hơn 700 người tham dự sự kiện này tại số 81 Vũ Trọng Phụng, Hà Nội.",
     "email":"iridescentprom2016@gmail.com",
-    "phone":"1-555-222-333",
+    "phone":"Uber, Ybox",
     "fb":"https://www.facebook.com/IridescentProm2016",
     "web":"http://promcnh.github.io/"
   },
   {  
-    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/alice.jpg",
-    "name":"Carla",
-    "last":"Doe",
-    "email":"carladoe@site.com",
-    "phone":"1-333-111-555",
-	"fb":"http://jhonSmith.com",
-    "web":"http://carladoe.com"
+    "photo":"img/top15.jpg",
+    "name":"The Original Project 2015",
+    "last":"cuộc thi làm film",
+    "email":"seventhartclub@gmail.com",
+    "phone":"NaN",
+    "fb":"https://www.facebook.com/CNH.SAC/",
+    "web":"https://www.facebook.com/CNH.SAC/"
   },
   {  
-    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/ALF.jpg",
-    "name":"Stefan",
-    "last":"Varela",
-    "email":"stefano@site.com",
-    "phone":"2-444-222-777",
-	"fb":"http://jhonSmith.com",
-    "web":"http://stefano.com"
-  },
-  {  
-    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/alice.jpg",
-    "name":"Angeline",
-    "last":"Julai",
-    "email":"angeline@site.com",
-    "phone":"2-555-555-555",
-	"fb":"http://jhonSmith.com",
-    "web":"http://angeline.com"
-  },
-  {  
-    "photo":"https://dl.dropboxusercontent.com/u/23834858/fotos/ALF.jpg",
-    "name":"New name",
-    "last":"for test",
-    "email":"angeline@site.com",
-    "phone":"2-555-555-555",
-	"fb":"http://jhonSmith.com",
-    "web":"http://angeline.com"
+    "photo":"img/carpe.jpg",
+    "name":"Carpe Diem Prom 2015",
+    "last":"prom",
+    "email":"seventhartclub@gmail.com",
+    "phone":"NaN",
+	"fb":"https://www.facebook.com/events/1443170629312880/",
+    "web":"https://www.facebook.com/events/1443170629312880/"
   }
 ]
