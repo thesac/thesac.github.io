f0f1f33a493113f613357e7776fc26ea1648094e
ngxson <thichthat@gmail.com>
2016-06-15 12:57:34 +0700

    products: more details

--- next/products/data.js
+++ next/products/data.js
@@ -1,9 +1,10 @@
 [
   {  
     "photo":"img/irid.jpg",
-    "name":"Iridescent Prom 2016",
+    "name":"Prom “Iridescent”",
 	"date":"28/5/2016",
     "last":"Prom năm thứ hai với nhiều kinh nghiệm và nội dung được trau chuốt kĩ lưỡng hơn.",
+    "detail":" - Thành công xây dựng một kịch bản đáp ứng đủ các tiêu chí về nội dung, nghệ thuật cũng như bảo đảm đủ sự tương tác giữa chương trình với người đến dự.</br> - Được nhận khá nhiều lời khen về thái độ phục vụ chuyên nghiệp của BTC.</br> - Nhận được sự tài trợ độc quyền của UBER, giảm sự khó khăn trong việc đi lại cho các khách dự prom.",
     "email":"iridescentprom2016@gmail.com",
     "phone":"Vũ Hương Giang",
     "fb":"https://www.facebook.com/IridescentProm2016",
@@ -11,9 +12,10 @@
   },
   {  
     "photo":"img/top15.jpg",
-    "name":"The Original Project 2015",
+    "name":"T.O.P ( The Original Project )",
 	"date":"Từ tháng 12/2015 đến 15/1/2016",
     "last":"Dự án làm phim ngắn đã thu hút được không chỉ học sinh khối chuyên trong trường, các nhóm làm phim lẻ mà cả những đối tượng ở ngoài trường.",
+	"detail":" - Nhận được những sản phẩm được đầu tư tốt về nội dung, âm thanh cũng như kĩ thuật, thành công truyền tải ý nghĩa mà chương trình muốn hướng đến.</br> - Đã đưa được tầm ảnh hưởng của SAC ra rộng hơn, thu hút mọi người đến với câu lạc bộ.",
     "email":"seventhartclub@gmail.com",
     "phone":"Vũ Quốc Việt, Ngô Thị Thục Yến",
     "fb":"https://www.facebook.com/CNH.SAC/",
@@ -21,9 +23,10 @@
   },
   {  
     "photo":"img/carpe.jpg",
-    "name":"Carpe Diem Prom 2015",
+    "name":"Prom “Carpe Diem”",
 	"date":"28/5/2016",
     "last":"Prom năm đầu tiên, xây dựng thành công chương trình nhạc kịch “Sảnh đường ước mơ”",
+	"detail":" - Số vé ( 600 vé ) phát ra đều được bán hết, thu hút sự chú ý của không chỉ học sinh trong trường mà còn cả ngoài trường và các đơn vị truyền thông.</br> - Thành công truyền tải được slogan “Nắm trọn khoảnh khắc”, xây dựng một kịch bản chương trình đầu tư kĩ lưỡng về nội dung, âm nhạc, chất lượng.",
     "email":"seventhartclub@gmail.com",
     "phone":"Đỗ Hải Minh, Đặng Hương Giang",
 	"fb":"https://www.facebook.com/events/1443170629312880/",
