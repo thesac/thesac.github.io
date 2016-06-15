b4930906b272232f07824fd6b7a5fb55dc1d86b9
ngxson <thichthat@gmail.com>
2016-06-15 23:48:46 +0700

    products: update now

--- next/products/data.js
+++ next/products/data.js
@@ -22,6 +22,17 @@
     "web":"no"
   },
   {  
+    "photo":"img/gsce.jpg",
+    "name":"Chương trình từ thiện “Nụ cười giáng sinh”",
+	"date":"20/12/2015",
+    "last":"Chương trình từ thiện quy mô tầm trung đầu tiên do SAC tổ chức, đã thu hút được sự chú ý của không chỉ các bạn trong trường mà còn các đơn vị ngoài trường, tạo nên một mối quan tâm lan rộng về hoàn cảnh các bệnh nhi ung thư ở bệnh viện K3, Tân Triều, Hà Nội.",
+	"detail":"( SAC kết hợp với CLB Nụ Cười - tổ chức thiện nguyện với sứ mệnh chia sẻ và mang lại tiếng cười cho những em nhỏ có hoàn cảnh đặc biệt, khó khăn thông qua chuỗi chương trình “Sáng mãi nụ cười em”.)</br> - Qũy hoàn toàn được gây dựng bằng cách bán đi những sản phẩm do ban Kỹ thuật của SAC làm như: búp bê vải, móc khóa,… thân thiện với môi trường với mức giá phải chăng.</br> - Chương trình đã thu lại được một số tiền khá lớn từ những lòng hảo tâm của các bạn học sinh. SAC thành công gây dựng được một kịch bản buổi từ thiện trực tiếp đầy đủ cả về mặt vật chất và tinh thần. Các tiết mục / hoạt động đều được xây dựng một cách công phu, chu đáo, thành công tặng cho các bệnh nhi một món quà tinh thần vô cùng quý giá.</br> - Cách làm việc của SAC được đánh giá là chuyên nghiệp, tỉ mỉ trong nội dung và cẩn thận trong hành động (sau sự kiện, SAC đã ở lại và dọn dẹp bất kì những rác bẩn, vỏ thức ăn còn lưu lại, … )</br> - Thành công truyền tải được thông điệp của sự kiện mà SAC muốn gửi gắm “Nghệ thuật bắt nguồn từ nhân văn”.",
+    "email":"seventhartclub@gmail.com",
+    "phone":"Đỗ Hải Minh (SAC kết hợp với CLB Nụ Cười)",
+	"fb":"https://www.facebook.com/events/526668304155998/",
+    "web":"no"
+  },
+  {  
     "photo":"img/carpe.jpg",
     "name":"Prom “Carpe Diem”",
 	"date":"28/5/2016",
