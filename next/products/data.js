3f8d7f789cb44b8336b838b0f7afbf7821f377da
ngxson <thichthat@gmail.com>
2016-06-15 12:52:10 +0700

    products: update

--- next/products/data.js
+++ next/products/data.js
@@ -2,27 +2,30 @@
   {  
     "photo":"img/irid.jpg",
     "name":"Iridescent Prom 2016",
-    "last":"Prom thường niên do SAC tổ chức. Sự kiện được thực hiện với sự giúp đỡ của hơn 40 tình nguyện viên. Đã có hơn 700 người tham dự sự kiện này tại số 81 Vũ Trọng Phụng, Hà Nội.",
+	"date":"28/5/2016",
+    "last":"Prom năm thứ hai với nhiều kinh nghiệm và nội dung được trau chuốt kĩ lưỡng hơn.",
     "email":"iridescentprom2016@gmail.com",
-    "phone":"Uber, Ybox",
+    "phone":"Vũ Hương Giang",
     "fb":"https://www.facebook.com/IridescentProm2016",
     "web":"http://promcnh.github.io/"
   },
   {  
     "photo":"img/top15.jpg",
     "name":"The Original Project 2015",
-    "last":"cuộc thi làm film",
+	"date":"Từ tháng 12/2015 đến 15/1/2016",
+    "last":"Dự án làm phim ngắn đã thu hút được không chỉ học sinh khối chuyên trong trường, các nhóm làm phim lẻ mà cả những đối tượng ở ngoài trường.",
     "email":"seventhartclub@gmail.com",
-    "phone":"NaN",
+    "phone":"Vũ Quốc Việt, Ngô Thị Thục Yến",
     "fb":"https://www.facebook.com/CNH.SAC/",
     "web":"https://www.facebook.com/CNH.SAC/"
   },
   {  
     "photo":"img/carpe.jpg",
     "name":"Carpe Diem Prom 2015",
-    "last":"prom",
+	"date":"28/5/2016",
+    "last":"Prom năm đầu tiên, xây dựng thành công chương trình nhạc kịch “Sảnh đường ước mơ”",
     "email":"seventhartclub@gmail.com",
-    "phone":"NaN",
+    "phone":"Đỗ Hải Minh, Đặng Hương Giang",
 	"fb":"https://www.facebook.com/events/1443170629312880/",
     "web":"https://www.facebook.com/events/1443170629312880/"
   }
