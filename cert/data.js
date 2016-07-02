c46c34de34e2dfc8fb0d139e5b45ec7c653cc3e9
ngxson <thichthat@gmail.com>
2016-07-03 00:15:11 +0700

    cert: test data

---
+++ cert/data.js
@@ -0,0 +1,18 @@
+[
+  {  
+    "photo":"img/irid.jpg",
+    "name":"Prom “Iridescent”",
+	"date":"28/5/2016",
+    "sum":"Prom năm thứ hai với nhiều kinh nghiệm và nội dung được trau chuốt kĩ lưỡng hơn.",
+    "leader":"Vũ Hương Giang",
+    "link":"http://promcnh.github.io/"
+  },
+  {  
+    "photo":"img/top15.jpg",
+    "name":"T.O.P (The Original Project)",
+	"date":"Từ tháng 12/2015 đến 15/1/2016",
+    "sum":"Dự án làm phim ngắn đã thu hút được không chỉ học sinh khối chuyên trong trường, các nhóm làm phim lẻ mà cả những đối tượng ở ngoài trường.",
+    "leader":"Vũ Quốc Việt, Ngô Thị Thục Yến",
+    "link":"no"
+  },
+]
