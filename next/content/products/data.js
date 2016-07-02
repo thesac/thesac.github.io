3f4282f8fa6a4c79669f7c1ecfad03be80cab592
ngxson <thichthat@gmail.com>
2016-07-02 19:06:04 +0700

    merge them now

---
+++ next/content/products/data.js
@@ -0,0 +1,46 @@
+[
+  {  
+    "photo":"img/irid.jpg",
+    "name":"Prom “Iridescent”",
+	"date":"28/5/2016",
+    "last":"Prom năm thứ hai với nhiều kinh nghiệm và nội dung được trau chuốt kĩ lưỡng hơn.",
+    "detail":" - Thành công xây dựng một kịch bản đáp ứng đủ các tiêu chí về nội dung, nghệ thuật cũng như bảo đảm đủ sự tương tác giữa chương trình với người đến dự.</br> - Được nhận khá nhiều lời khen về thái độ phục vụ chuyên nghiệp của BTC.</br> - Nhận được sự tài trợ độc quyền của UBER, giảm sự khó khăn trong việc đi lại cho các khách dự prom.",
+    "email":"iridescentprom2016@gmail.com",
+    "phone":"Vũ Hương Giang",
+    "fb":"https://www.facebook.com/IridescentProm2016",
+    "web":"http://promcnh.github.io/"
+  },
+  {  
+    "photo":"img/top15.jpg",
+    "name":"T.O.P (The Original Project)",
+	"date":"Từ tháng 12/2015 đến 15/1/2016",
+    "last":"Dự án làm phim ngắn đã thu hút được không chỉ học sinh khối chuyên trong trường, các nhóm làm phim lẻ mà cả những đối tượng ở ngoài trường.",
+	"detail":" - Nhận được những sản phẩm được đầu tư tốt về nội dung, âm thanh cũng như kĩ thuật, thành công truyền tải ý nghĩa mà chương trình muốn hướng đến.</br> - Đã đưa được tầm ảnh hưởng của SAC ra rộng hơn, thu hút mọi người đến với câu lạc bộ.",
+    "email":"seventhartclub@gmail.com",
+    "phone":"Vũ Quốc Việt, Ngô Thị Thục Yến",
+    "fb":"https://www.facebook.com/CNH.SAC/",
+    "web":"no"
+  },
+  {  
+    "photo":"img/gsce.jpg",
+    "name":"Chương trình từ thiện “Nụ cười giáng sinh”",
+	"date":"20/12/2015",
+    "last":"Chương trình từ thiện quy mô tầm trung đầu tiên do SAC tổ chức, đã thu hút được sự chú ý của không chỉ các bạn trong trường mà còn các đơn vị ngoài trường, tạo nên một mối quan tâm lan rộng về hoàn cảnh các bệnh nhi ung thư ở bệnh viện K3, Tân Triều, Hà Nội.",
+	"detail":"( SAC kết hợp với CLB Nụ Cười - tổ chức thiện nguyện với sứ mệnh chia sẻ và mang lại tiếng cười cho những em nhỏ có hoàn cảnh đặc biệt, khó khăn thông qua chuỗi chương trình “Sáng mãi nụ cười em”.)</br> - Qũy hoàn toàn được gây dựng bằng cách bán đi những sản phẩm do ban Kỹ thuật của SAC làm như: búp bê vải, móc khóa,… thân thiện với môi trường với mức giá phải chăng.</br> - Chương trình đã thu lại được một số tiền khá lớn từ những lòng hảo tâm của các bạn học sinh. SAC thành công gây dựng được một kịch bản buổi từ thiện trực tiếp đầy đủ cả về mặt vật chất và tinh thần. Các tiết mục / hoạt động đều được xây dựng một cách công phu, chu đáo, thành công tặng cho các bệnh nhi một món quà tinh thần vô cùng quý giá.</br> - Cách làm việc của SAC được đánh giá là chuyên nghiệp, tỉ mỉ trong nội dung và cẩn thận trong hành động (sau sự kiện, SAC đã ở lại và dọn dẹp bất kì những rác bẩn, vỏ thức ăn còn lưu lại, … )</br> - Thành công truyền tải được thông điệp của sự kiện mà SAC muốn gửi gắm “Nghệ thuật bắt nguồn từ nhân văn”.",
+    "email":"seventhartclub@gmail.com",
+    "phone":"Đỗ Hải Minh (SAC kết hợp với CLB Nụ Cười)",
+	"fb":"https://www.facebook.com/events/526668304155998/",
+    "web":"no"
+  },
+  {  
+    "photo":"img/carpe.jpg",
+    "name":"Prom “Carpe Diem”",
+	"date":"28/5/2016",
+    "last":"Prom năm đầu tiên, xây dựng thành công chương trình nhạc kịch “Sảnh đường ước mơ”",
+	"detail":" - Số vé ( 600 vé ) phát ra đều được bán hết, thu hút sự chú ý của không chỉ học sinh trong trường mà còn cả ngoài trường và các đơn vị truyền thông.</br> - Thành công truyền tải được slogan “Nắm trọn khoảnh khắc”, xây dựng một kịch bản chương trình đầu tư kĩ lưỡng về nội dung, âm nhạc, chất lượng.",
+    "email":"seventhartclub@gmail.com",
+    "phone":"Đỗ Hải Minh, Đặng Hương Giang",
+	"fb":"https://www.facebook.com/events/1443170629312880/",
+    "web":"no"
+  }
+]
