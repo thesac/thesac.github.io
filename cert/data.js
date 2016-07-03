210be58932936662bfdac9a91873920bd9867f27
ngxson <thichthat@gmail.com>
2016-07-03 10:28:25 +0700

    cert: prepair now!

--- cert/data.js
+++ cert/data.js
@@ -5,14 +5,14 @@
 	"date":"28/5/2016",
     "sum":"Prom năm thứ hai với nhiều kinh nghiệm và nội dung được trau chuốt kĩ lưỡng hơn.",
     "leader":"Vũ Hương Giang",
-    "link":"http://promcnh.github.io/"
+    "link":"http://promcnh.github.io/verify"
   },
   {  
     "photo":"img/top15.jpg",
     "name":"T.O.P (The Original Project)",
-	"date":"Từ tháng 12/2015 đến 15/1/2016",
+	"date":"From December 2015 to January 2016",
     "sum":"Dự án làm phim ngắn đã thu hút được không chỉ học sinh khối chuyên trong trường, các nhóm làm phim lẻ mà cả những đối tượng ở ngoài trường.",
     "leader":"Vũ Quốc Việt, Ngô Thị Thục Yến",
-    "link":"no"
+    "link":"/cert/top"
   }
 ]
