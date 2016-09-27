08b6e8738b7de5b50938dce1d82b1c8abdb2ee9c
ngxson <thichthat@gmail.com>
2016-09-27 12:43:41 +0700

    top: result up date js

--- next/content/top/result/js/top_score.js
+++ next/content/top/result/js/top_score.js
@@ -7,11 +7,11 @@ var postData = [null,
 ["Nhóm 6: K.Y.S","<b>- Nhẹ tựa nỗi đau -</b></br>bộ phim kể về 1 cô bé bị mù tên Bình và 1 cô gái bị điên tên Linh đều là những con người bất hạnh . Họ tìm được nhau và nhìn thấy niềm hạnh phúc trong đó","9IBQHdUGFnU","","",-1],
 ["Nhóm 7: SIPTEAM","<b>- Nắng phía chân cầu -</b></br>\"Nắng Phía Chân Cầu\" là 1 bộ phim ngắn kể về Khanh, 1 cô bé sinh ra và lớn lên trong 1 gia đình khá giả. Cuộc đời của cô là 1 chuỗi những ngày tháng sung túc cho tới khi sóng gió xảy ra với gia đình cô. Với những suy nghĩ nông nổi, Khanh đã tự dồn mình vào tuyệt vọng. Nhưng tình thương đã giúp Khanh vượt qua tất cả, và hơn hết, nó giúp cô thay đổi cách nhìn về mọi thứ xung quanh, giúp cô tìm lại cho mình mục đính sống đáng quý. Thông điệp muốn truyền tải qua bộ phim: niềm vui đôi khi chỉ đơn giản là hạnh phúc của những người xung quanh. Sau cơn mưa trời lại nắng. Đem lại niềm vui cho mọi người cũng là tìm cho mình một nguồn hạnh phúc, để cho tâm hồn luôn nhẹ nhàng, thanh thản, luôn vững vàng trước mọi sóng gió trong cuộc sống.","SrWImBW3VCs","","",-1],
 ["Nhóm 8: A1 k41 KL","<b>- Sống là cho đi -</b></br>Bộ phim kể về cuộc đời của một cậu bé nhặt ve chai (mẹ mất do bị ung thư còn bố mất khi cậu chưa ra đời) có một trái tim ấm áp. Rất nhiều biến cố xảy ra với cậu nhg cuối cùng cậu được một người đàn bà hiền hậu cưu mang và giúp cậu được đi học. Thông điệp chúng tôi muốn gửi đến chính là chúng ta hãy cho đi tình thương của chúng ta đến những người xung quanh ngay cả những người chúng ta ko quen biết khi họ gặp khó khăn. SỐNG LÀ CHO ĐI ĐÂU CHỈ NHẬN CHO MÌNH.","ie6ly9v0DLE","","",-1],
-["Nhóm 9: THE RET TEAM","<b>- A better me -</b></br>The problem is not the problem, the problem is your attitude about the problem.","","","",-1],
+["Nhóm 9: THE RET TEAM","<b>- A better me -</b></br>The problem is not the problem, the problem is your attitude about the problem.","QBbZFw1G9hA","","",-1],
 ["Nhóm 10: Súc Sích production","<b>- To Cyra -</b></br>Họ đánh mất nhau, và rồi họ kiếm tìm. Họ lần theo nhưng dấu vết trong cố gắng và hi vọng. Nhưng đằng sau đó là cả một nỗi bi kịch, là những bí mật, những xúc cảm và ký ức không thể lý giải được bằng cách nào khác ngoài sự chia sẻ và cảm thông giữa hai tâm hồn đồng điệu. Liệu thông điệp có được gửi đi, hay tình yêu vẫn mãi chỉ còn lại là những manh mối?","LyMu7tBUb5g","","",-1],
 ["Nhóm 11: K&C Team","<b>- Friends -</b></br>Trên cuộc đời này, có thể nói là tình bạn là thứ tình cảm quan trọng nhất, thậm chí là quan trọng hơn cả tình cảm gia đình hay là tình cảm nam nữ. Con người ta có thể sống mười mấy năm cuộc đời thiếu tình cảm trai gái, có thể lớn lên rồi sống xa bố mẹ. Nhưng trong suốt những năm tháng đó, con người không thể nào thiếu được những người bạn. Và bộ phim này là cái nhìn nhận của K&amp;C Team về tình bạn, về bạn bè. Qua bộ phim này, điều mà bọn mình muốn nhắn gửi tới mọi người chính là “Tình bạn là thứ vô cùng thiêng liêng và đừng bao giờ để bất cứ thứ gì làm sứt mẻ đi tình bạn của bạn.”","vgu1UdgVzLA","","",-1],
 ["Nhóm 12: SONDER GROUP","<b>- Giấy -</b></br>Con người ta, ai sinh ra cũng như một tờ giấy Con người ta, ai sinh ra cũng như một tờ giấy trắng, trong sáng và vô lo. Thế nhưng giấy không trắng mãi được, giấy sinh ra cho màu sắc, cho nét bút, cho hình khối. Đôi khi những mảnh tối xuất hiện. Và đôi khi, những thứ bất ngờ như chút tình cảm chớm nở, một cuộc gặp gỡ người bạn cũ lại đem tới những màu sắc rực rỡ không ngờ.</br>Cuộc đời cũng hữu hạn như diện tích vuông giấy, đừng lãng phí dù chỉ một milimets. Hãy để những cảm xúc trong sáng xuất phát từ trái tim trở thành những mảng màu tươi sáng điểm tô cuộc đời mình!</br>\"Giấy\" mang trong mình thông điệp ấy, truyền tải qua câu chuyện của Quang và Minh, hai con người, hai số phận không may mắn vô tình được gắn kết với nhau, nảy sinh những tình cảm đẹp đẽ để rồi gắn kết và hoàn thiện lẫn nhau...","nCdk2Bi7HvM","","",-1],
-["Nhóm 13: Maverick","<b>-  -</b></br>","","","",-1]
+["Nhóm 13: Maverick","<b>- Hành Trình Kiêu Hãnh -</b></br>Bộ phim kể về cuộc sống của 2 cô gái đảo lộn khi bố Phương lấy mẹ Thảo. Phương ước mơ, hoài bão nhưng bệnh tim lại ngăn cản tuổi trẻ của cô. Cô ghét sự thương hại của mọi người, ghét bàn tay giúp đỡ của Thảo. Tức giận, ganh ghét, cảm phục, xúc động, hạnh phúc . 2 cô gái đều cùng nhau trải qua.Phương ra đi nhưng cô và Thảo đã thực hiện cuộc hành trình kiêu hãnh, đong đầy nước mắt , đong đầy tình yêu thương ","","","",-1]
 ];
 
 var numOfTeams = 14;
@@ -93,6 +93,12 @@ function initTop(pid) {
 		if ((msyt != -1) && (msfblike != -1) && (msfbshare != -1)) {
 			score.innerHTML = mscore + ' điểm';
 			postData[pid][5] = mscore;
+			console.log("Team"+pid+
+					" yt="+msyt+
+					" fb="+msfblike+
+					" share="+msfbshare+
+					" total"+mscore);
+			getTop();
 		}
     });
 	
@@ -105,6 +111,12 @@ function initTop(pid) {
 		if ((msyt != -1) && (msfblike != -1) && (msfbshare != -1)) {
 			score.innerHTML = mscore + ' điểm';
 			postData[pid][5] = mscore;
+			console.log("Team"+pid+
+					" yt="+msyt+
+					" fb="+msfblike+
+					" share="+msfbshare+
+					" total"+mscore);
+			getTop();
 		}
     });
 }
