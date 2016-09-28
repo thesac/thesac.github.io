c9ceb234f7da38ef999cdc255d36825c142e9462
ngxson <thichthat@gmail.com>
2016-09-28 18:32:23 +0700

    top: result: finalize

--- next/content/top/result/js/top_score.js
+++ next/content/top/result/js/top_score.js
@@ -1,30 +1,31 @@
 var postData = [null,
-["Nhóm 1: T.B.Đ Team","<b>- The Letter -</b></br>","","","",-1],
-["Nhóm 2: The Illumination","<b>- Điều có thể -</b></br>Bạn có tin vào ĐỊNH MỆNH không? Câu nói ấy đã thôi thúc chúng tôi làm nên bộ phim này . Một bộ phim về tình người với tình yêu đồng giới , tình yêu nam nữ nà tình phụ tử thiêng liêng . \"Trên thế giới này , mọi điều không thể đều có thể xảy ra\" . Câu nói ấy đã gắn kết Thanh và Linh , cũng chinh câu nói ấy đã làm Luật dao động . Và cuối cùng , Thanh đã…","xYFZlDupyX0","","",-1],
-["Nhóm 3: A PDP production","<b>- Nắng -</b></br>Bộ phim là dòng cảm xúc của một cô gái mồ côi mẹ từ nhỏ, cũng chính vì thế mà bố của cô đã có những hành động không đúng đắn vs chính con gái của mình. Vì vậy, cô gái luôn thu mình với tất cả mọi người. Nhưng trái tim nhỏ bé ấy đã dần đc mở lòng và được sưởi ấm khi có đc sự động viên chia sẻ của một người bạn.Từ lúc chỉ mới biết cô là bạn cùng lớp đến khi biết những gì cô phải chịu đựng, cậu bạn ấy vẫn luôn ở bên và giúp đỡ cô gái ấy. Bộ phim lên án hành vi thiếu đạo đức, qua đó đề cao một thứ tình cảm cao quý - Tình người. Tình người tình thương luôn diễn ra xung quanh ta...","rNS0m1JRvJc","","",-1],
-["Nhóm 4: Ngắn gọn thôi","<b>- Hà Nội cô đơn -</b></br>Bộ phim xoay quay về tình cảm con người và những gì sâu kín nhất trong lòng thủ đô.","ldMU7zfcp8g","","",-1],
-["Nhóm 5: SHALLT MTAND","<b>- Người tốt -</b></br>Câu chuyện kể về một cậu học sinh với bản tính hiền lành, trong sáng thích giúp đỡ mọi người xung quanh, nhưng vì những nỗi buồn và bất hạnh ập đến liên tục khiến cậu chán nản và dần tìm ra bản ngã của chính mình.","QQyd2lIBRxQ","","",-1],
-["Nhóm 6: K.Y.S","<b>- Nhẹ tựa nỗi đau -</b></br>bộ phim kể về 1 cô bé bị mù tên Bình và 1 cô gái bị điên tên Linh đều là những con người bất hạnh . Họ tìm được nhau và nhìn thấy niềm hạnh phúc trong đó","9IBQHdUGFnU","","",-1],
-["Nhóm 7: SIPTEAM","<b>- Nắng phía chân cầu -</b></br>\"Nắng Phía Chân Cầu\" là 1 bộ phim ngắn kể về Khanh, 1 cô bé sinh ra và lớn lên trong 1 gia đình khá giả. Cuộc đời của cô là 1 chuỗi những ngày tháng sung túc cho tới khi sóng gió xảy ra với gia đình cô. Với những suy nghĩ nông nổi, Khanh đã tự dồn mình vào tuyệt vọng. Nhưng tình thương đã giúp Khanh vượt qua tất cả, và hơn hết, nó giúp cô thay đổi cách nhìn về mọi thứ xung quanh, giúp cô tìm lại cho mình mục đính sống đáng quý. Thông điệp muốn truyền tải qua bộ phim: niềm vui đôi khi chỉ đơn giản là hạnh phúc của những người xung quanh. Sau cơn mưa trời lại nắng. Đem lại niềm vui cho mọi người cũng là tìm cho mình một nguồn hạnh phúc, để cho tâm hồn luôn nhẹ nhàng, thanh thản, luôn vững vàng trước mọi sóng gió trong cuộc sống.","SrWImBW3VCs","","",-1],
-["Nhóm 8: A1 k41 KL","<b>- Sống là cho đi -</b></br>Bộ phim kể về cuộc đời của một cậu bé nhặt ve chai (mẹ mất do bị ung thư còn bố mất khi cậu chưa ra đời) có một trái tim ấm áp. Rất nhiều biến cố xảy ra với cậu nhg cuối cùng cậu được một người đàn bà hiền hậu cưu mang và giúp cậu được đi học. Thông điệp chúng tôi muốn gửi đến chính là chúng ta hãy cho đi tình thương của chúng ta đến những người xung quanh ngay cả những người chúng ta ko quen biết khi họ gặp khó khăn. SỐNG LÀ CHO ĐI ĐÂU CHỈ NHẬN CHO MÌNH.","ie6ly9v0DLE","","",-1],
-["Nhóm 9: THE RET TEAM","<b>- A better me -</b></br>The problem is not the problem, the problem is your attitude about the problem.","QBbZFw1G9hA","","",-1],
-["Nhóm 10: Súc Sích production","<b>- To Cyra -</b></br>Họ đánh mất nhau, và rồi họ kiếm tìm. Họ lần theo nhưng dấu vết trong cố gắng và hi vọng. Nhưng đằng sau đó là cả một nỗi bi kịch, là những bí mật, những xúc cảm và ký ức không thể lý giải được bằng cách nào khác ngoài sự chia sẻ và cảm thông giữa hai tâm hồn đồng điệu. Liệu thông điệp có được gửi đi, hay tình yêu vẫn mãi chỉ còn lại là những manh mối?","LyMu7tBUb5g","","",-1],
-["Nhóm 11: K&C Team","<b>- Friends -</b></br>Trên cuộc đời này, có thể nói là tình bạn là thứ tình cảm quan trọng nhất, thậm chí là quan trọng hơn cả tình cảm gia đình hay là tình cảm nam nữ. Con người ta có thể sống mười mấy năm cuộc đời thiếu tình cảm trai gái, có thể lớn lên rồi sống xa bố mẹ. Nhưng trong suốt những năm tháng đó, con người không thể nào thiếu được những người bạn. Và bộ phim này là cái nhìn nhận của K&amp;C Team về tình bạn, về bạn bè. Qua bộ phim này, điều mà bọn mình muốn nhắn gửi tới mọi người chính là “Tình bạn là thứ vô cùng thiêng liêng và đừng bao giờ để bất cứ thứ gì làm sứt mẻ đi tình bạn của bạn.”","vgu1UdgVzLA","","",-1],
-["Nhóm 12: SONDER GROUP","<b>- Giấy -</b></br>Con người ta, ai sinh ra cũng như một tờ giấy Con người ta, ai sinh ra cũng như một tờ giấy trắng, trong sáng và vô lo. Thế nhưng giấy không trắng mãi được, giấy sinh ra cho màu sắc, cho nét bút, cho hình khối. Đôi khi những mảnh tối xuất hiện. Và đôi khi, những thứ bất ngờ như chút tình cảm chớm nở, một cuộc gặp gỡ người bạn cũ lại đem tới những màu sắc rực rỡ không ngờ.</br>Cuộc đời cũng hữu hạn như diện tích vuông giấy, đừng lãng phí dù chỉ một milimets. Hãy để những cảm xúc trong sáng xuất phát từ trái tim trở thành những mảng màu tươi sáng điểm tô cuộc đời mình!</br>\"Giấy\" mang trong mình thông điệp ấy, truyền tải qua câu chuyện của Quang và Minh, hai con người, hai số phận không may mắn vô tình được gắn kết với nhau, nảy sinh những tình cảm đẹp đẽ để rồi gắn kết và hoàn thiện lẫn nhau...","nCdk2Bi7HvM","","",-1],
-["Nhóm 13: Maverick","<b>- Hành Trình Kiêu Hãnh -</b></br>Bộ phim kể về cuộc sống của 2 cô gái đảo lộn khi bố Phương lấy mẹ Thảo. Phương ước mơ, hoài bão nhưng bệnh tim lại ngăn cản tuổi trẻ của cô. Cô ghét sự thương hại của mọi người, ghét bàn tay giúp đỡ của Thảo. Tức giận, ganh ghét, cảm phục, xúc động, hạnh phúc . 2 cô gái đều cùng nhau trải qua.Phương ra đi nhưng cô và Thảo đã thực hiện cuộc hành trình kiêu hãnh, đong đầy nước mắt , đong đầy tình yêu thương ","","","",-1]
+null,
+["Nhóm 2: The Illumination","<b>- Điều có thể -</b></br>Bạn có tin vào ĐỊNH MỆNH không? Câu nói ấy đã thôi thúc chúng tôi làm nên bộ phim này . Một bộ phim về tình người với tình yêu đồng giới , tình yêu nam nữ nà tình phụ tử thiêng liêng . \"Trên thế giới này , mọi điều không thể đều có thể xảy ra\" . Câu nói ấy đã gắn kết Thanh và Linh , cũng chinh câu nói ấy đã làm Luật dao động . Và cuối cùng , Thanh đã…","i2eEmw9UsQ8","1808871722730718_1857202684564288","",-1],
+["Nhóm 3: A PDP production","<b>- Nắng -</b></br>Bộ phim là dòng cảm xúc của một cô gái mồ côi mẹ từ nhỏ, cũng chính vì thế mà bố của cô đã có những hành động không đúng đắn vs chính con gái của mình. Vì vậy, cô gái luôn thu mình với tất cả mọi người. Nhưng trái tim nhỏ bé ấy đã dần đc mở lòng và được sưởi ấm khi có đc sự động viên chia sẻ của một người bạn.Từ lúc chỉ mới biết cô là bạn cùng lớp đến khi biết những gì cô phải chịu đựng, cậu bạn ấy vẫn luôn ở bên và giúp đỡ cô gái ấy. Bộ phim lên án hành vi thiếu đạo đức, qua đó đề cao một thứ tình cảm cao quý - Tình người. Tình người tình thương luôn diễn ra xung quanh ta...","rNS0m1JRvJc","1808871722730718_1857203927897497","",-1],
+["Nhóm 4: Ngắn gọn thôi","<b>- Hà Nội cô đơn -</b></br>Bộ phim xoay quay về tình cảm con người và những gì sâu kín nhất trong lòng thủ đô.","ldMU7zfcp8g","1808871722730718_1857204607897429","",-1],
+["Nhóm 5: SHALLT MTAND","<b>- Người tốt -</b></br>Câu chuyện kể về một cậu học sinh với bản tính hiền lành, trong sáng thích giúp đỡ mọi người xung quanh, nhưng vì những nỗi buồn và bất hạnh ập đến liên tục khiến cậu chán nản và dần tìm ra bản ngã của chính mình.","QQyd2lIBRxQ","1808871722730718_1857205321230691","",-1],
+["Nhóm 6: K.Y.S","<b>- Nhẹ tựa nỗi đau -</b></br>bộ phim kể về 1 cô bé bị mù tên Bình và 1 cô gái bị điên tên Linh đều là những con người bất hạnh . Họ tìm được nhau và nhìn thấy niềm hạnh phúc trong đó","9IBQHdUGFnU","1808871722730718_1857205694563987","",-1],
+["Nhóm 7: SIPTEAM","<b>- Nắng phía chân cầu -</b></br>\"Nắng Phía Chân Cầu\" là 1 bộ phim ngắn kể về Khanh, 1 cô bé sinh ra và lớn lên trong 1 gia đình khá giả. Cuộc đời của cô là 1 chuỗi những ngày tháng sung túc cho tới khi sóng gió xảy ra với gia đình cô. Với những suy nghĩ nông nổi, Khanh đã tự dồn mình vào tuyệt vọng. Nhưng tình thương đã giúp Khanh vượt qua tất cả, và hơn hết, nó giúp cô thay đổi cách nhìn về mọi thứ xung quanh, giúp cô tìm lại cho mình mục đính sống đáng quý. Thông điệp muốn truyền tải qua bộ phim: niềm vui đôi khi chỉ đơn giản là hạnh phúc của những người xung quanh. Sau cơn mưa trời lại nắng. Đem lại niềm vui cho mọi người cũng là tìm cho mình một nguồn hạnh phúc, để cho tâm hồn luôn nhẹ nhàng, thanh thản, luôn vững vàng trước mọi sóng gió trong cuộc sống.","SrWImBW3VCs","1808871722730718_1857206974563859","",-1],
+["Nhóm 8: A1 k41 KL","<b>- Sống là cho đi -</b></br>Bộ phim kể về cuộc đời của một cậu bé nhặt ve chai (mẹ mất do bị ung thư còn bố mất khi cậu chưa ra đời) có một trái tim ấm áp. Rất nhiều biến cố xảy ra với cậu nhg cuối cùng cậu được một người đàn bà hiền hậu cưu mang và giúp cậu được đi học. Thông điệp chúng tôi muốn gửi đến chính là chúng ta hãy cho đi tình thương của chúng ta đến những người xung quanh ngay cả những người chúng ta ko quen biết khi họ gặp khó khăn. SỐNG LÀ CHO ĐI ĐÂU CHỈ NHẬN CHO MÌNH.","ie6ly9v0DLE","1808871722730718_1857207727897117","",-1],
+["Nhóm 9: THE RET TEAM","<b>- A better me -</b></br>The problem is not the problem, the problem is your attitude about the problem.","QBbZFw1G9hA","1808871722730718_1857208261230397","",-1],
+["Nhóm 10: Súc Sích production","<b>- To Cyra -</b></br>Họ đánh mất nhau, và rồi họ kiếm tìm. Họ lần theo nhưng dấu vết trong cố gắng và hi vọng. Nhưng đằng sau đó là cả một nỗi bi kịch, là những bí mật, những xúc cảm và ký ức không thể lý giải được bằng cách nào khác ngoài sự chia sẻ và cảm thông giữa hai tâm hồn đồng điệu. Liệu thông điệp có được gửi đi, hay tình yêu vẫn mãi chỉ còn lại là những manh mối?","QD9cpZ8YCdw","1808871722730718_1857211911230032","",-1],
+["Nhóm 11: K&C Team","<b>- Friends -</b></br>Trên cuộc đời này, có thể nói là tình bạn là thứ tình cảm quan trọng nhất, thậm chí là quan trọng hơn cả tình cảm gia đình hay là tình cảm nam nữ. Con người ta có thể sống mười mấy năm cuộc đời thiếu tình cảm trai gái, có thể lớn lên rồi sống xa bố mẹ. Nhưng trong suốt những năm tháng đó, con người không thể nào thiếu được những người bạn. Và bộ phim này là cái nhìn nhận của K&amp;C Team về tình bạn, về bạn bè. Qua bộ phim này, điều mà bọn mình muốn nhắn gửi tới mọi người chính là “Tình bạn là thứ vô cùng thiêng liêng và đừng bao giờ để bất cứ thứ gì làm sứt mẻ đi tình bạn của bạn.”","vgu1UdgVzLA","1808871722730718_1857212381229985","",-1],
+["Nhóm 12: SONDER GROUP","<b>- Giấy -</b></br>Con người ta, ai sinh ra cũng như một tờ giấy Con người ta, ai sinh ra cũng như một tờ giấy trắng, trong sáng và vô lo. Thế nhưng giấy không trắng mãi được, giấy sinh ra cho màu sắc, cho nét bút, cho hình khối. Đôi khi những mảnh tối xuất hiện. Và đôi khi, những thứ bất ngờ như chút tình cảm chớm nở, một cuộc gặp gỡ người bạn cũ lại đem tới những màu sắc rực rỡ không ngờ.</br>Cuộc đời cũng hữu hạn như diện tích vuông giấy, đừng lãng phí dù chỉ một milimets. Hãy để những cảm xúc trong sáng xuất phát từ trái tim trở thành những mảng màu tươi sáng điểm tô cuộc đời mình!</br>\"Giấy\" mang trong mình thông điệp ấy, truyền tải qua câu chuyện của Quang và Minh, hai con người, hai số phận không may mắn vô tình được gắn kết với nhau, nảy sinh những tình cảm đẹp đẽ để rồi gắn kết và hoàn thiện lẫn nhau...","nCdk2Bi7HvM","1808871722730718_1857213397896550","",-1],
+["Nhóm 13: Maverick","<b>- Hành Trình Kiêu Hãnh -</b></br>Bộ phim kể về cuộc sống của 2 cô gái đảo lộn khi bố Phương lấy mẹ Thảo. Phương ước mơ, hoài bão nhưng bệnh tim lại ngăn cản tuổi trẻ của cô. Cô ghét sự thương hại của mọi người, ghét bàn tay giúp đỡ của Thảo. Tức giận, ganh ghét, cảm phục, xúc động, hạnh phúc . 2 cô gái đều cùng nhau trải qua.Phương ra đi nhưng cô và Thảo đã thực hiện cuộc hành trình kiêu hãnh, đong đầy nước mắt , đong đầy tình yêu thương ","4YDUWDBj0Hg","1808871722730718_1857213907896499","",-1]
+
 ];
 
 var numOfTeams = 14;
 
 var i;
-for(i=1 ; i < numOfTeams ; i++) {
+for(i=2 ; i < numOfTeams ; i++) {
 	initTop(i);
 }
 
 function updateTop() {
 	var j;
 	var ok = true;
-	for(j=1 ; j < numOfTeams ; j++) {
+	for(j=2 ; j < numOfTeams ; j++) {
 		if(postData[j][5] == -1) ok = false;
 	}
 	if(ok) {
@@ -39,27 +40,27 @@ function getTop() {
 	a = 0;
 	b = 0;
 	c = 0;
-	for(j=1 ; j < numOfTeams ; j++) {
+	for(j=2 ; j < numOfTeams ; j++) {
 		if(postData[j][5] > a) {
 			a = postData[j][5];
 			x = j;
 		}
 	}
-	for(j=1 ; j < numOfTeams ; j++) {
+	for(j=2 ; j < numOfTeams ; j++) {
 		if(postData[j][5] > b && postData[j][5] != a) {
 			b = postData[j][5];
 			y = j;
 		}
 	}
-	for(j=1 ; j < numOfTeams ; j++) {
+	for(j=2 ; j < numOfTeams ; j++) {
 		if(postData[j][5] > c && postData[j][5] != b && postData[j][5] != a) {
 			c = postData[j][5];
 			z = j;
 		}
 	}
-	var toptop1 = document.getElementById('toptop1'); toptop1.innerHTML = postData[x][0];
-	var toptop2 = document.getElementById('toptop2'); toptop2.innerHTML = postData[y][0];
-	var toptop3 = document.getElementById('toptop3'); toptop3.innerHTML = postData[z][0];
+	var toptop1 = document.getElementById('toptop1'); toptop1.innerHTML = "1. "+postData[x][0] +": " + postData[x][5] +" điểm";
+	var toptop2 = document.getElementById('toptop2'); toptop2.innerHTML = "2. "+postData[y][0] +": " + postData[y][5] +" điểm";
+	var toptop3 = document.getElementById('toptop3'); toptop3.innerHTML = "3. "+postData[z][0] +": " + postData[z][5] +" điểm";
 }
 
 
@@ -103,11 +104,11 @@ function initTop(pid) {
     });
 	
 	//facebook
-	$.getJSON('http://graph.facebook.com/'+idfb+'?fields=likes.summary(true).limit(0),shares', function(data) {
-        msfblike = +(data.likes.summary.total_count);
+	$.getJSON('https://graph.facebook.com/v2.7/'+idfb+'?access_token=EAAIZBAcdXlxYBAHoI9hZC72DEuY6TJ0P8CoOlo3QjUdBSql292ZCfufYTnHlLwuww9vQWUgFXV0vDmu9HiQ6MNWJNitJPa7bVsh7rRwxfTP6ZCXkGCJEHoWEqMh0PVjpb0OUOGvNDkx1LMrh030UX9gzYkPCcIYyuuoJKvma1QZDZD&fields=reactions.summary(true)%2Cshares&format=json&method=get&pretty=0', function(data) {
+        msfblike = +(data.reactions.summary.total_count);
         msfbshare = +(data.shares.count);
 		mscore = msyt+msfblike*2+msfbshare*3;
-		sfb.innerHTML = msfblike + ' likes,' + msfbshare + ' shares';
+		sfb.innerHTML = msfblike + ' likes+reactions,' + msfbshare + ' shares';
 		if ((msyt != -1) && (msfblike != -1) && (msfbshare != -1)) {
 			score.innerHTML = mscore + ' điểm';
 			postData[pid][5] = mscore;
